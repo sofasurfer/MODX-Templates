096bd69c80d38cd6d6672b51f8d5a64da94dadfb
kib <kib@sofabook.local>
2017-04-08 14:24:31 +0200

    Add studde yeah

---
+++ assets/templates/studeyea/js/main.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+    
+
+    /* Show tooltips*/
+    $('[data-toggle="tooltip"]').tooltip()
+
+    /* Enable swipe on carousel */
+    $('.carousel').bcSwipe({ threshold: 50 });
+
+});

a39f91dcf82771784f7ec23d7a17b3f52947bdde
sofasurfer <kilian@sofasurfer.ch>
2018-10-12 16:10:56 +0200

    Update image gallery

--- assets/templates/sofaweb/js/default.js
+++ assets/templates/sofaweb/js/default.js
@@ -1,95 +1,4 @@
 
-/*
- * Bootstrap Image Gallery 3.0.1
- * https://github.com/blueimp/Bootstrap-Image-Gallery
- *
- * Copyright 2013, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- */
-
-/*global define, window */
-
-(function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define([
-            'jquery',
-            './blueimp-gallery'
-        ], factory);
-    } else {
-        factory(
-            window.jQuery,
-            window.blueimp.Gallery
-        );
-    }
-}(function ($, Gallery) {
-    'use strict';
-
-    $.extend(Gallery.prototype.options, {
-        useBootstrapModal: true
-    });
-
-    var close = Gallery.prototype.close,
-        imageFactory = Gallery.prototype.imageFactory,
-        videoFactory = Gallery.prototype.videoFactory,
-        textFactory = Gallery.prototype.textFactory;
-
-    $.extend(Gallery.prototype, {
-
-        modalFactory: function (obj, callback, factoryInterface, factory) {
-            if (!this.options.useBootstrapModal || factoryInterface) {
-                return factory.call(this, obj, callback, factoryInterface);
-            }
-            var that = this,
-                modalTemplate = this.container.children('.modal'),
-                modal = modalTemplate.clone().show()
-                    .on('click', function (event) {
-                        // Close modal if click is outside of modal-content:
-                        if (event.target === modal[0] ||
-                                event.target === modal.children()[0]) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                            that.close();
-                        }
-                    }),
-                element = factory.call(this, obj, function (event) {
-                    callback({
-                        type: event.type,
-                        target: modal[0]
-                    });
-                    modal.addClass('in');
-                }, factoryInterface);
-            modal.find('.modal-title').text(element.title || String.fromCharCode(160));
-            modal.find('.modal-body').append(element);
-            return modal[0];
-        },
-
-        imageFactory: function (obj, callback, factoryInterface) {
-            return this.modalFactory(obj, callback, factoryInterface, imageFactory);
-        },
-
-        videoFactory: function (obj, callback, factoryInterface) {
-            return this.modalFactory(obj, callback, factoryInterface, videoFactory);
-        },
-
-        textFactory: function (obj, callback, factoryInterface) {
-            return this.modalFactory(obj, callback, factoryInterface, textFactory);
-        },
-
-        close: function () {
-            this.container.find('.modal').removeClass('in');
-            close.call(this);
-        }
-
-    });
-
-}));
-
-
-
 function setCookie(key, value) {
     var expires = new Date();
     expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
@@ -198,27 +107,14 @@ $(document).ready(function() {
   /*
     Image gallery stuff
   */
-  var fullscreenOptions = {
-      // Defines if the gallery should open in fullscreen mode:
-      fullScreen: true
-  };
-  $( "#links" ).click(function(event) {
+  document.getElementById('blueimp-gallery-images').onclick = function (event) {     
       event = event || window.event;
       var target = event.target || event.srcElement,
           link = target.src ? target.parentNode : target,
-          options = {fullScreen: true, index: link, event: event},
-          links = this.getElementsByTagName('a');
+          options = {index: link, event: event},
+          links = this.getElementsByTagName('a');    
       blueimp.Gallery(links, options);
-  });
-  
-  $( "#links btn" ).text('');
-
-  $('.item-hover').hover( function() {
-      $(this).find('.item-hover-caption').fadeIn(300);
-  }, function() {
-      $(this).find('.item-hover-caption').fadeOut(100);
-  })
-
+  };
 
   /*
     GitHub Widget
--- core/components/bstrap/elements/chunks/tpl.cookiebanner.tpl
+++ core/components/bstrap/elements/chunks/tpl.cookiebanner.tpl
@@ -1,6 +1,6 @@
 
 
 <div id="cookiebanner">
-    <a href="#" id="closecookiebanner">X</a>
+    <a href="#" id="closecookiebanner">ok</a>
     <p>I use cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies. <a href="[[~1171]]">More info</a></p>
 </div>
